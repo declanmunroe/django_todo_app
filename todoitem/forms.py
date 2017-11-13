60f39b9d56c48b6944e435486d9471dbce080258
Declan <decofeco29@gmail.com>
2017-11-13 12:12:46 +0000

    Adding Travis

--- todoitem/forms.py
+++ todoitem/forms.py
@@ -4,6 +4,6 @@ from .models import TodoItem
 class TodoItemForm(forms.ModelForm):
     class Meta:
         model = TodoItem
-        fields = ('name', 'priority', 'done')
+        fields = ('name', 'done')
         
 #forms.py is just a django shortcut of creating a form so you dont have to fill up your index .html with a contact form.
--- todoitem/models.py
+++ todoitem/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class TodoItem(models.Model):
     name = models.CharField(max_length=30, blank=False)
     done = models.BooleanField(blank=False, default=False)
-    priority = models.CharField(max_length=6, blank=False)
+    priority = models.CharField(max_length=6, blank=False, default="medium")
     
     def __str__(self): #this functions tells the admin panel in django administration what to display. In this case the name
         return self.name
